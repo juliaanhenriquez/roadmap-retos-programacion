957593f221ee39fd90b3d2d5a7abf94d07dd54d5
Julian Henriquez <juhenriquez@alumnos.uai.cl>
2026-04-21 20:55:55 -0400

    Add JavaScript example with variables and data types

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/juliaanhenriquez.js
@@ -0,0 +1,29 @@
+
+// Sitio web oficial de JavaScript (MDN)
+// https://developer.mozilla.org/es/docs/Web/JavaScript 
+
+/*
+este es un comentario de 
+varias lineas en JavaScript
+*/
+
+// variable 
+let nombre = "variable";
+
+// constante (no cambia)
+const version = 2024;
+
+// Tipos de datos primitivos
+
+let texto = "Hola mundo"; //string
+let numero = 10; // number
+let decimal = 3.14; //number
+let veredadero = true; //boolean
+let falso = false; //boolean 
+let indefinido; //undifined
+let nulo = null; //null
+
+// Imprimir en consola
+
+console.log("Hola!, ") + nombre + "!"
+
